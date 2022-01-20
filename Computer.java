7b205a5e90db8d84988256fef7fb2d7781628b60
e1b18049@oit.ac.jp <e1b18049@st.oit.ac.jp>
2022-01-20 14:26:17 +0900

    Computerの⼿の決定とその表⽰

--- Computer.java
+++ Computer.java
@@ -2,9 +2,11 @@ import java.util.Random;
 
 public class Computer {
     public static int decidesComputerHand() {
-        //課題3 Computerの手の決定とその表示
-        //Randomメソッドを使って1~3の数値を返す
+        // 課題3 Computerの手の決定とその表示
+        Random rand = new Random();
 
+        int computerHand = rand.nextInt(3) + 1;
 
+        return computerHand;
     }
 }
